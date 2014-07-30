42a2167745e1a270957ae2c07caf2e16f5cd4f69
Sukhdeep Singh Handa <sukhdeep1991@gmail.com>
2014-07-31 03:16:49 +0530

    Paypal plugin integrated. Yet not working

--- www/js/Controllers/upgrade-controller.js
+++ www/js/Controllers/upgrade-controller.js
@@ -3,7 +3,37 @@ angular
 .controller(
 		'upgradeCtrl',
 function($scope) {
+	var config = new PayPalConfiguration({
+		merchantName: "AppToDate", 
+		merchantPrivacyPolicyURL: "https://mytestshop.com/policy", 
+		merchantUserAgreementURL: "https://mytestshop.com/agreement"
+	});
+	
+	var clientIDs = {
+	    "PayPalEnvironmentSandbox": appConfig.paypalSanboxId
+	  };
+	var createPayment = function () {
+      // for simplicity use predefined amount
+      var paymentDetails = new PayPalPaymentDetails("1.50", "0.40", "0.05");
+      var payment = new PayPalPayment("1.95", "USD", "AppToDate activation", "Sale", paymentDetails);
+      return payment;
+    }
+	
+	PayPalMobile.init(clientIDs, onPayPalMobileInit);
+	
+	var onPayPalMobileInit = function(){
+		// must be called
+	      // use PayPalEnvironmentNoNetwork mode to get look and feel of the flow
+	      PayPalMobile.prepareToRender("PayPalEnvironmentNoNetwork", config, null);
+	}
+			
 	$scope.upgrade = function(){
-		alert('Not implemented');
+		// single payment
+        PayPalMobile.renderSinglePaymentUI(createPayment(), 
+			function(success){
+        	 console.log("Payment was successfull: " + JSON.stringify(success));
+        }, function(error){
+        	console.log("Payment canceled by user: " + JSON.stringify(error));
+        });
 	}
 });
--- www/js/config.js
+++ www/js/config.js
@@ -3,7 +3,9 @@ appConfig = {
 	apiUrl : "http://192.168.1.180/AppToDate.WebHost/api/",
 	accessToken: "testAccessToken",
 	inviteMessage: "You have been invited to use AppToDate. Please visit www.google.com to register.",
-	googleSenderId: "196925764211"
+	googleSenderId: "196925764211",
+	paypalProductionId: "AXlh5BCdP3d-g3S4xLGDTfXiZwZ2SLTtqxFE-U9kpPGsPVQf_aJBy4J3LJyr",
+	paypalSanboxId: "AXlh5BCdP3d-g3S4xLGDTfXiZwZ2SLTtqxFE-U9kpPGsPVQf_aJBy4J3LJyr"
 };
 
 eventStatus = {
