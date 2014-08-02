73189d8b223e1c111b5bf41447088ded63c48edd
Sukhdeep Singh Handa <sukhdeep1991@gmail.com>
2014-08-02 14:14:36 +0530

    Upgrade page changes for paypal

--- www/js/Controllers/upgrade-controller.js
+++ www/js/Controllers/upgrade-controller.js
@@ -10,23 +10,22 @@ function($scope) {
 	});
 	
 	var clientIDs = {
+	    "PayPalEnvironmentProduction": appConfig.paypalSanboxId,
 	    "PayPalEnvironmentSandbox": appConfig.paypalSanboxId
 	  };
-	var createPayment = function () {
-      // for simplicity use predefined amount
-      var paymentDetails = new PayPalPaymentDetails("1.50", "0.40", "0.05");
-      var payment = new PayPalPayment("1.95", "USD", "AppToDate activation", "Sale", paymentDetails);
-      return payment;
-    }
 	
 	PayPalMobile.init(clientIDs, onPayPalMobileInit);
 	
 	var onPayPalMobileInit = function(){
 		// must be called
 	      // use PayPalEnvironmentNoNetwork mode to get look and feel of the flow
-	      PayPalMobile.prepareToRender("PayPalEnvironmentNoNetwork", config, null);
+	      PayPalMobile.prepareToRender("PayPalEnvironmentNoNetwork", config, onPrepareRender);
 	}
 			
+	var onPrepareRender = function(){
+		
+	}
+	
 	$scope.upgrade = function(){
 		// single payment
         PayPalMobile.renderSinglePaymentUI(createPayment(), 
@@ -36,4 +35,10 @@ function($scope) {
         	console.log("Payment canceled by user: " + JSON.stringify(error));
         });
 	}
+	var createPayment = function () {
+      // for simplicity use predefined amount
+      var paymentDetails = new PayPalPaymentDetails("1.50", "0.40", "0.05");
+      var payment = new PayPalPayment("1.95", "USD", "AppToDate activation", "Sale", paymentDetails);
+      return payment;
+    }
 });
