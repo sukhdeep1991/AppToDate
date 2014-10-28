fd0701b498f0571cb194ac866e78f85ffb880456
ssh <sukhdeep1991@gmail.com>
2014-10-28 23:38:42 +0530

    Added new url for API

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,7 @@
 appConfig = {
-	apiUrl : "http://54.200.127.24/AppToDate.WebHost/api/",
+	//apiUrl : "http://54.200.127.24/AppToDate.WebHost/api/",
 	//apiUrl : "http://192.168.1.180/AppToDate.WebHost/api/",
+	apiUrl : "http://ec2-54-187-15-232.us-west-2.compute.amazonaws.com/AppToDate.WebHost/api/",
 	accessToken: "testAccessToken",
 	inviteMessage: "You have been invited to use AppToDate. Please visit www.google.com to register.",
 	googleSenderId: "196925764211",
